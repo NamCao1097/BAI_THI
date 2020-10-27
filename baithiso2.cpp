fa363a270adffc7692507740a7468e28113d76b5
NamCao1097 <72310456+NamCao1097@users.noreply.github.com>
2020-10-27 20:06:46 +0700

    Update baithiso2.cpp

--- baithiso2.cpp
+++ baithiso2.cpp
@@ -9,10 +9,10 @@ int main(){
 		printf("nhap so arr[%d] = ",i);
 		scanf("%d",&arr[i]);
 	}
-	float a=0;
+	float a = 0;
 	for(int i=0;i<n;i++){
 		a+=arr[i];
 	}
-	float TBC=a/n;
+	float TBC = a/n;
 	printf("TBC cua mang la: %f",TBC);
 }
